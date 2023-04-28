12458c20556697e46c03c9261bc5b58ecdb65f07
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-28 10:39:14 -0300

    feat: add os países de forma interativa

--- exercicíoArray/src/App.java
+++ exercicíoArray/src/App.java
@@ -1,6 +1,11 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
         String [] countries = new String[4];
-        
+
+        for (int index = 0; index < 4; index += 1) {
+            countries[index] = JOptionPane.showInputDialog("Digite o nome de um país");
+        }
     }
 }
