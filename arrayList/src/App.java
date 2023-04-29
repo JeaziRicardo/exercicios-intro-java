e25bc5fb3a296869cfac6f85f8db98d85e383124
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-29 20:05:06 -0300

    feat: adiciona carros no ArrayList

--- arrayList/src/App.java
+++ arrayList/src/App.java
@@ -3,5 +3,10 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
         ArrayList<String> cars = new ArrayList<String>();
+
+        cars.add("Volvo XC90");
+        cars.add("BMW X5");
+        cars.add("Ford Mustang");
+        cars.add("Fiat Uno");
     }
 }
