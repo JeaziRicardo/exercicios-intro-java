fe33546c9bb0a24f4893cdeb4b4e674bbf3c2871
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-29 19:59:17 -0300

    feat: cria o ArrayList para cars

--- arrayList/src/App.java
+++ arrayList/src/App.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<String> cars = new ArrayList<String>();
     }
 }
