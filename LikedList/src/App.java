778f886025731828a24e310167d75a60791f7027
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-29 21:44:23 -0300

    feat: cria o LinkedList cars

--- LikedList/src/App.java
+++ LikedList/src/App.java
@@ -1,5 +1,7 @@
+import java.util.LinkedList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        LinkedList<String> cars = new LinkedList<String>();
     }
 }
