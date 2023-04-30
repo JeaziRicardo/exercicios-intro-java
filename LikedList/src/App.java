178012906868db094db9f152d96247dadf9ae47c
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-29 21:48:58 -0300

    feat: adiciona carros a lista

--- LikedList/src/App.java
+++ LikedList/src/App.java
@@ -3,5 +3,9 @@ import java.util.LinkedList;
 public class App {
     public static void main(String[] args) throws Exception {
         LinkedList<String> cars = new LinkedList<String>();
+
+        cars.add("Ford Mustang");
+        cars.add("Shelby Cobra");
+        cars.add("Chevrolet Camaro");
     }
 }
